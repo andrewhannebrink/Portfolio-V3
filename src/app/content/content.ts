e6a327aba503d3d27ca2f18a096fad6da35b6bbe
Andrew Hannebrink <andrewhannebrink@gmail.com>
2018-02-05 18:34:27 -0800

    added some more professional experience bullet point content

--- src/app/content/content.ts
+++ src/app/content/content.ts
@@ -57,7 +57,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2015',
       scope: 'Image Processing, Full-Stack Web Development, API Construction, UI/UX Design, SaaS, Server-Side Automation',
     technologies: 'Angular 1, Express, Python, PIL, HTML, Canvas, CSS',
-    description: ''
+    description: "●\n\n●\n\n●\n\n●"
   },
   {
     id: 3,
@@ -78,7 +78,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2015',
     scope: 'Video Art, Euclidean Geometry, Matrix Algebra, Object Oriented Programming',
     technologies: 'Python, Blender, bpl (Blender-Python Module)',
-    description: ''
+    description: ""
   },
   {
     id: 4,
@@ -98,7 +98,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2015 - Present',
     scope: 'Image Processing, Viral Marketing, Back-End Web Development, Twitter API, Server-Side Automation',
     technologies: 'JavaScript, Node, Python, PIL, AWS, EC2, Crontab',
-    description: ''
+    description: "● Engineered high-traffic Twitter bot (~15k tweets/month, >100k lifetime tweets, >10k followers) that processes emoji photo-mosaics of each image tweeted to it using Python Imaging Library and npm Twit module.\n\n● Rate-limited tweets and queued them in MongoDB for delayed reply when experiencing high-traffic."
   },
   {
     id: 5,
@@ -118,7 +118,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2014',
     scope: 'Video Processing, Video Art, System Process Automation',
     technologies: 'Python, PIL, ffmpeg',
-    description: ''
+    description: "●\n\n●\n\n●\n\n●"
   },
   {
     id: 6,
@@ -134,7 +134,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2014',
     scope: 'Network Engineering, Server-Side Scripting, Database Administration, Automation, Command Line Tooling',
     technologies: 'Python, SQL, Infoblox API',
-    description: ''
+    description: ""
   },
   {
     id: 7,
@@ -150,7 +150,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2014',
     scope: 'Network Engineering, Server-Side Scripting, Database Administration, Automation, Command Line Tooling',
     technologies: 'Python, SQL, Cisco Controller API',
-    description: ''
+    description: "   These are Python scripts I wrote for Washington University's system administrators and security analysts. In 2014, the university’s Network Operating Center was overseeing a shift from Meru to Cisco wireless routing equipment. During this transition, though, they found themselves writing a mess of repetitive commands across the Cisco and Meru API’s for performance monitoring. To ease this pain, I wrote these scripts to automate the process of session lookup across networks. After completion, instead of repeatedly signing in out of convoluted 3rd-party CLI’s everytime we wanted to perform a network check, we could perform dynamic user lookups for either Meru or Cisco systems all from one command-line system. Used over the course of a year as we completed the transition to Cisco equipment, these scripts saved the university an estimated $25,000 in labor time."
   },
   {
     id: 8,
@@ -177,7 +177,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2016 - 2017',
     scope: '',
     technologies: '',
-    description: ''
+    description: "● Rapidly delivered features for the world’s 2nd largest dating platform with Angular, Node.js, and Google Closure UI/Library for desktop and mobile browsers in a continuous integration development cycle.\n\n● Implemented country-specific UI experiments that simplified profile interactions and improved user experience, resulting in 30% more accepted greetings, and 7% more bookings.\n\n● Led technical makeover of our Help Center which curtailed customer support emails, saving us from hiring a full-time employee, while still dynamically supplying users contact methods in 23 languages across 39 locales.\n\n● Won 1st place in company Hack Day by allowing users to donate to charities during special in-app purchases."
   },
   {
     id: 10,
@@ -188,7 +188,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2016',
     scope: '',
     technologies: '',
-    description: ''
+    description: "● Worked in a 6-engineer team to develop a web and iOS app for video conferencing, live document and image sharing, and synchronized video streaming using AngularJS, WebRTC via Licode, and UI-Bootstrap.\n\n● Greatly reduced vectors for XSS attacks by 99.9% by architecting app-wide form validation using combination of custom and existing Angular directives for sanitizing inputs and whitelisting characters.\n\n● Followed JavaScript OOP principles to create module for panning and zooming through content in real time.\n\n● Migrated front-end build and unit test processes from Grunt to Gulp, enabling us to build with ES6 via Babel.\n\n● Constructed E2E and unit tests for deployed API instances using Jasmine, automating the tasks with Grunt.\n\n● Enabled users to switch cameras, microphones, and external speakers using the WebRTC API.\n\n● Implemented in-browser keyboard shortcuts for navigating menus, browsing content, and controlling videos."
   },
   {
     id: 11,
@@ -199,7 +199,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: '2014 - 2015',
     scope: '',
     technologies: '',
-    description: ''
+    description: "● Followed scrum agile development methodology in a team of three engineers using Jira, and Pivotal Tracker to build a web dashboard for customers to visualize speed improvements of their mobile applications.\n\n● Developed RESTful user management API for new customer dashboard account activation, sign-in, sign-out, and password recovery using Python, Django and the Django Rest Framework.\n\n● Built custom Angular controllers and services for communication with user management and mobile data API’s.\n\n● Implemented user authentication with CSRF tokens, session cookies, and email password recovery.\n\n● Created Node.js API endpoints to dynamically build SQL queries to supply D3.js chart data, visualising mobile data pertaining to device type, network type, time interval, status codes, region, and apps.\n\n● Deployed Redis caching layer to decrease page load-time due to costly SQL queries, cutting load times from >20 seconds to <1 second for clients with ~100gb of data.\n\n● Improved internal response time to key customer events through a Jenkins-monitored email report system with a Python script pulling data from MongoDB, PostgreSQL, and Amazon S3, saving the company ~$20,000 yearly."
   },
   {
     id: 12,
@@ -210,7 +210,7 @@ export const POSSIBLE_CONTENT: Content[] = [
     year: null,
     scope: null, 
     technologies: null,
-    description: ''
+    description: ""
   }
 ];
 
